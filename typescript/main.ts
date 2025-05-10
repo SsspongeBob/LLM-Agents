a7dfb613496636e8e2561a11067b1688c4ad69eb
mr-mario <3301748996@qq.com>
2025-05-10 12:52:56 +0800

    modified:   typescript/main.ts

--- typescript/main.ts
+++ typescript/main.ts
@@ -1,15 +1,7 @@
-// import { LLM } from "./agents/llm/llm";
 import { config } from "dotenv";
+import { ChatOpenAI } from "@langchain/openai";
 
 config({ override: true })
-
-// ; (async () => {
-//   const llm = new LLM("llama-3-8b-instruct-chinese");
-//   const result = await llm.generate("你好");
-//   console.log(result);
-// })();
-
-import { ChatOpenAI } from "@langchain/openai";
 
 const openai = new ChatOpenAI({ model: "gpt-4o-mini" })
 
